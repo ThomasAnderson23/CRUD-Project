ba429c6357fb37fca474994e8b1e62718c26c3ee
Rodrigo Navarrete <renavarr@uc.cl>
2022-10-27 22:35:43 -0300

    Added new select input

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+
+showUser()
 let nameInput = document.getElementById("nameInput");
+let emailInput = document.getElementById("emailInput");
 let submitButton = document.getElementById("submitButton");
+let levelSelect = document.getElementById("levelSelect");
 
 submitButton.addEventListener('click', e => {
 
     const nameInputValue = nameInput.value;
+    const emailInputValue = emailInput.value;
+    const levelSelectValue = levelSelect.value;
+
+
+
 
     if (nameInputValue) {
         let webUser = localStorage.getItem("localUser")
@@ -15,16 +24,38 @@ submitButton.addEventListener('click', e => {
         }
 
         userObject.push({
-            'user': nameInputValue
+            'user': nameInputValue,
+            'email': emailInputValue,
+            'level': levelSelectValue,
         });
 
         localStorage.setItem("localUser", JSON.stringify(userObject));
         nameInput.value = "";
     }
+    showUser()
 });
 
 
-function showUser(){
-    let webUser = localStorage.getItem("localUSer");
-    
+function showUser() {
+    let webUser = localStorage.getItem("localUser");
+    if (webUser == null){
+        userObject = [];
+    } else {
+        userObject = JSON.parse(webUser);
+    }
+    let html = "";
+    let addedUserList = document.getElementById("addedUserList");
+    userObject.forEach((item, index) => {
+        html += `<tr>
+                    <th scope="col">${index+1}</th>
+                    <td>${item.user} </td>
+                    <td>${item.email} </td>
+                    <td>${item.level} </td>
+                        <td><button type="button onclick="editUser(${index})" class="text-primary">Edit</button></td>
+
+                        <td><button type="button onclick="editUser(${index})" class="text-danger">Delete</button></td>
+                    </tr>`;
+    });
+
+    addedUserList.innerHTML = html;
 }

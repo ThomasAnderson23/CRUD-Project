129de97d6d38942e7be5733d756164610d1a7c89
Rodrigo Navarrete <renavarr@uc.cl>
2022-10-28 20:40:25 -0300

    added update and delete features

--- app.js
+++ app.js
@@ -1,73 +1,101 @@
-
-showUser()
-editUser()
+showUser();
 let nameInput = document.getElementById("nameInput");
 let emailInput = document.getElementById("emailInput");
-let submitButton = document.getElementById("submitButton");
 let levelSelect = document.getElementById("levelSelect");
+let submitButton = document.getElementById("submitButton");
+let saveUserButton = document.getElementById("saveUserButton");
+let currentIndex = 0;
 
-submitButton.addEventListener('click', e => {
+submitButton.addEventListener("click", (e) => {
+  const nameInputValue = nameInput.value;
+  const emailInputValue = emailInput.value;
+  const levelSelectValue = levelSelect.value;
 
-    const nameInputValue = nameInput.value;
-    const emailInputValue = emailInput.value;
-    const levelSelectValue = levelSelect.value;
+  if (nameInputValue) {
+    let webUser = localStorage.getItem("localUser");
 
-    if (nameInputValue) {
-        let webUser = localStorage.getItem("localUser")
-
-        if (webUser == null) {
-            userObject = [];
-        } else {
-            userObject = JSON.parse(webUser)
-        }
-
-        userObject.push({
-            'user': nameInputValue,
-            'email': emailInputValue,
-            'level': levelSelectValue,
-        });
-
-        localStorage.setItem("localUser", JSON.stringify(userObject));
-        nameInput.value = "";
+    if (webUser == null) {
+      userObject = [];
+    } else {
+      userObject = JSON.parse(webUser);
     }
-    showUser()
-    editUser()
+
+    userObject.push({
+      user: nameInputValue,
+      email: emailInputValue,
+      level: levelSelectValue,
+    });
+
+    localStorage.setItem("localUser", JSON.stringify(userObject));
+    nameInput.value = "";
+  }
 });
 
-
 function showUser() {
-    let webUser = localStorage.getItem("localUser");
-    if (webUser == null){
-        userObject = [];
-    } else {
-        userObject = JSON.parse(webUser);
-    }
-    let html = "";
-    let addedUserList = document.getElementById("addedUserList");
-    userObject.forEach((item, index) => {
-        html += `<tr>
-                    <th scope="col">${index+1}</th>
+  let webUser = localStorage.getItem("localUser");
+  if (webUser == null) {
+    userObject = [];
+  } else {
+    userObject = JSON.parse(webUser);
+  }
+  let html = "";
+  let addedUserList = document.getElementById("addedUserList");
+  userObject.forEach((item, index) => {
+    html += `<tr>
+                    <th scope="col">${index + 1}</th>
                     <td>${item.user} </td>
                     <td>${item.email} </td>
                     <td>${item.level} </td>
-                        <td><button type="button" onclick="editUser(${index})" class="text-primary">Edit</button></td>
+                        <td><button type="button" onclick="editUser(${index})" class="btn btn-primary btn-sm">Edit</button></td>
 
-                        <td><button type="button" onclick="editUser(${index})" class="text-danger">Delete</button></td>
+                        <td><button type="button" onclick="deleteUser(${index})" class="btn btn-danger btn-sm">Delete</button></td>
                     </tr>`;
-    });
+  });
 
-    addedUserList.innerHTML = html;
+  addedUserList.innerHTML = html;
 }
 
 function editUser(index) {
-    let nameInput = document.getElementById("nameInput");
-    let submitButton = document.getElementById("submitButton");
-    let saveUserButton = document.getElementById("saveUserButton"); 
-    let userObject = JSON.parse(webUser);
+  currentIndex = 0;
+  let webUser = localStorage.getItem("localUser");
+  let userObject = JSON.parse(webUser);
 
-    nameInput.value = index;
-    emailInputValue = userObject [index]['user'];
+  nameInput.value = userObject[index]["user"];
+  emailInput.value = userObject[index]["email"];
+  levelSelect.value = userObject[index]["level"];
 
-    submitButton.style.display="none;"
-    saveUserButton.style.diaplay="inline-block";
+  submitButton.style.display = "none";
+  saveUserButton.style.display = "inline-block";
+
+  currentIndex = index;
+  //añadir scroll up
 }
+
+saveUserButton.addEventListener("click", function (e) {
+  let webUser = localStorage.getItem("localUser");
+  let userObject = JSON.parse(webUser);
+
+  userObject[currentIndex].user = nameInput.value;
+  userObject[currentIndex].email = emailInput.value;
+  userObject[currentIndex].level = levelSelect.value;
+ 
+  submitButton.style.display = "none;";
+  saveUserButton.style.display = "inline-block";
+
+  localStorage.setItem("localUser", JSON.stringify(userObject));
+  nameInput.value = "";
+  emailInput.value = "";
+  levelSelect.value = "";
+  showUser();
+});
+
+function deleteUser(index){
+let webUser = localStorage.getItem("localUser");
+let userObject = JSON.parse(webUser);
+
+userObject.splice(index, 1);
+
+localStorage.setItem("localUser", JSON.stringify(userObject));
+showUser();
+}
+
